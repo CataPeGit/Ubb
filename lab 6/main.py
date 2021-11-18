dafc9c23766d5b6dae1b26eaa53ee47001c892a7
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2021-11-18 19:28:06 +0200

    updated main

--- lab 6/main.py
+++ lab 6/main.py
@@ -1,130 +1,67 @@
-# got problem 16
-
-class MyPoint:
-
-    def __init__(self, coord_x, coord_y, color):
-        self.__coord_x = coord_x
-        self.__coord_y = coord_y
-        self.__color = color
-
-    def __str__(self):
-        return f'Point ({self.__coord_x},{self.__coord_y}) of color {self.__color}.'
-
-    def getValueX(self):
-        return self.__coord_x
-
-    def getValueY(self):
-        return self.__coord_y
-
-    def getColor(self):
-        return self.__color
-
-    def setCoordX(self, new_coord_x):
-        self.__coord_x = new_coord_x
-
-    def setCoordY(self, new_coord_y):
-        self.__coord_y = new_coord_y
+from UI import *
+from classes import *
+from tests import *
+import matplotlib.pyplot as plt
 
 
-class PointRepository(MyPoint):
-
-    def __init__(self, repository=[]):
-        # super().__init__()
-        self.__repository = repository.copy()
-
-    def get_all_points(self):
-        # Returns a list of all points
-        return self.__repository
-
-    def add_point(self, new_point):
-        # adds a new point to the repository
-        self.__repository.append(new_point)
-
-    def shift_points_on_x_axis(self, way):
-        # we will shift the points on the x axis by adding 1 to the current x
-        if way == "+":
-            for point in self.__repository:
-                x = point.getValueX()
-                point.setCoordX(x + 1)
-        elif way == "-":
-            for point in self.__repository:
-                x = point.getValueX()
-                point.setCoordX(x - 1)
-
-    def get_point_at_index(self, index):
-        # Returns the point found at a given index
-        return self.__repository[index]
-
-    def points_of_given_color(self, given_color):
-        result_points = []
-        for point in self.__repository:
-            color = point.getColor()
-            if color == given_color:
-                result_points.append(point)
-        return result_points
-
-
-def commands():
-    print("0 -> Quit program")
-    print("1 -> Add a point to the repository")
-    print("2 -> Get all points")  # function 2
-    print("3 -> Get all points that are inside a given square")  # function 5
-    print("4 -> Get a point at a given index ")
-    print("5 -> Shift all points on the x axis")
-    print("6 -> Get all points of a given color")
+def command_valid(comm):
+    if comm >= 0 and comm <= 12:
+        return True
+    return False
 
 
 def menu():
 
     # we will create some starting points and add them to the repository
 
-    repository = PointRepository()
+    # repository.get_3_data_examples()
+    # repository.get_10_data_examples()
 
-    point1 = MyPoint(1, 2, "red")
-    point2 = MyPoint(4, 1, "green")
-    point3 = MyPoint(2, 7, "blue")
-
-    repository.add_point(point1)
-    repository.add_point(point2)
-    repository.add_point(point3)
-
-    """
-    points = repository.get_all_points()
-
-    print(points)
-    print()
-    print()
-    print()
-    print("------------")
-    print()
-    print()
-    """
     while True:
         commands()
 
         comm = int(input("Your command: "))
 
+        if command_valid(comm) == False:
+            print("Command does not exist! Please enter a valid one.")
+
         if comm == 0:
             quit()
 
         if comm == 1:
+            # Add a point to the repository
             print()
             print("New point information: ")
 
             new_x = int(input("x: "))
             new_y = int(input("y: "))
             new_color = input("Color: ")
-            new_point = MyPoint(new_x, new_y, new_color)
 
+            new_point = MyPoint(new_x, new_y, new_color)
             repository.add_point(new_point)
 
         if comm == 2:
+            # Get all points in the repository
             all_points = repository.get_all_points()
             for point in all_points:
                 print(point.__str__())
 
         if comm == 3:
-            # up-left corner and length given
+            # Get a point at a given index in the repository
+            index = int(input("Index: "))
+            searched_point = repository.get_point_at_index(index)
+            print(searched_point.__str__())
+
+        if comm == 4:
+            # Get all points of a given color in the repository
+            given_color = input("Color searched: ")
+            searched_points = repository.points_of_given_color(given_color)
+            for point in searched_points:
+                print(point)
+
+        if comm == 5:
+            # Get all points that are inside a given square from the repository
+            # Given the up-left corner and length of the square
             print("Up-left corner coordinates:")
             x_upLeftCorner = int(input("x: "))
             y_upLeftCorner = int(input("y: "))
@@ -133,21 +70,43 @@ def menu():
             print()
             print("Points are: ")
 
-            all_points = repository.get_all_points()
-            for point in all_points:
-                x = point.getValueX()
-                y = point.getValueY()
-                if (x >= x_upLeftCorner and
-                    x_upLeftCorner + length >= x and
-                        y >= y_upLeftCorner - length and y <= y_upLeftCorner):
-                    print(point.__str__())
+            repository.points_in_square(x_upLeftCorner, y_upLeftCorner, length)
 
-        if comm == 4:
-            index = int(input("Index: "))
-            searched_point = repository.get_point_at_index(index)
-            print(searched_point.__str__())
+        if comm == 6:
+            # Get the minimum distance between two points from the repository
+            index1 = int(input("First point index: "))
+            index2 = int(input("Second point index: "))
+            print(repository.get_distance_between_two_points(index1, index2))
 
-        if comm == 5:
+        if comm == 7:
+            # Update a point at a given index of the repository
+            index = int(input("index: "))
+            newX = int(input("New x: "))
+            newY = int(input("New y: "))
+            newColor = input("New color: ")
+            repository.update_point_at_index(index, newX, newY, newColor)
+
+        if comm == 8:
+            # Delete the point located at the given index of the repository
+            index = int(input("index: "))
+            repository.delete_point_at_index(index)
+
+        if comm == 9:
+            # Delete all points that are inside a given square from the repository
+            # Given the up-left corner and length of the square
+            print("Up-left corner coordinates:")
+            x_upLeftCorner = int(input("x: "))
+            y_upLeftCorner = int(input("y: "))
+            length = int(input("Side length: "))
+            repository.delete_point_in_square(
+                x_upLeftCorner, y_upLeftCorner, length)
+
+        if comm == 10:
+            # Plot all points in a chart using the matplotlib library
+            repository.plot_points_in_chart()
+
+        if comm == 11:
+            # Shift all points on the x axis in the desired way
             way = input("Type + for a rigth shift or - for decreasing x: ")
             repository.shift_points_on_x_axis(way)
             all_points = repository.get_all_points()
@@ -156,11 +115,18 @@ def menu():
             for point in all_points:
                 print(point.__str__())
 
-        if comm == 6:
-            given_color = input("Color searched: ")
-            searched_points = repository.points_of_given_color(given_color)
-            for point in searched_points:
-                print(point)
+        if comm == 12:
+            # test all functions
+            run_tests()
+            print("Tests are all good!")
+
+        if comm == 13:
+            repository.get_3_data_examples()
+            print("Examples added!")
+
+        if comm == 14:
+            repository.get_10_data_examples()
+            print("Examples added!")
 
         print()
         print()
@@ -170,68 +136,7 @@ def menu():
         print()
 
 
+repository = PointRepository()
 menu()
-
-
-"""
-
-
-
-
-
----
-
-    repository.append(MyPoint(1, 2, "red"))
-    repository.append(MyPoint(1, 5, "blue"))
-    repository.append(MyPoint(4, 3, "green"))
-
-    while True:
-
-        comm = int(input("Your command: "))
-
-        if comm == 0:
-            quit()
-
-        if comm == 1:
-
-
-
-def commands():
-    print("0 -> Quit program")
-    print("1 -> Get and set the value of all properties for a point")
-    print("2 -> Provide the representation of a point")
-
-
-
----
-
-
-    print("Point coordinates:")
-    x = int(input("X: "))
-    y = int(input("Y: "))
-    color = input("Color: ")
-
-    point = MyPoint(x, y, color)
-
-    print()
-    commands()
-    comm = int(input("Your command: "))
-
-    if comm == 0:
-        quit()
-
-    if comm == 1:
-        x = int(input("New x coordinate:"))
-        MyPoint.__setCoordX(point.__coord_x)
-
-        print(point.__coord_x)
-
-        print("New y coordinate:")
-        MyPoint.__setCoordY(point.__coord_y)
-
-    if comm == 2:
-        print(MyPoint.__str__(point))
-
-     """
 
 
