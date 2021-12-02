59663471b7ab815833b488908e968c07c321b31e
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2021-12-02 20:26:46 +0200

    Updated classes

--- Assignment 4/classes.py
+++ Assignment 4/classes.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 
 class MyVector:
@@ -29,7 +31,7 @@ class MyVector:
         """
         getter method - get the type
         """
-        return self.__type
+        return self.__typeInteger
 
     def get_values(self):
         """
@@ -53,7 +55,7 @@ class MyVector:
         """
         setter method - set the type
         """
-        self.__type = new_type
+        self.__typeInteger = new_type
 
     def set_values(self, new_value):
         """
@@ -70,6 +72,11 @@ class MyVector:
 
     def add(self, vector_to_add):
         # adds the values with "vector_to_add"
+        if len(vector_to_add) != len(self.__values):
+            print("Vectors have different lengths.")
+            print()
+            return
+
         i = 0
         while i < len(self.__values):
             self.__values[i] += vector_to_add[i]
@@ -77,6 +84,11 @@ class MyVector:
 
     def substract(self, vector_to_add):
         # substracts the values with "vector_to_add"
+        if len(vector_to_add) != len(self.__values):
+            print("Vectors have different lengths.")
+            print()
+            return
+
         i = 0
         while i < len(self.__values):
             self.__values[i] -= vector_to_add[i]
@@ -84,6 +96,16 @@ class MyVector:
 
     def multiplication(self, vector_to_add):
         # multiplication of the values with "vector_to_add"
+        if len(vector_to_add) != len(self.__values):
+            print("Vectors have different lengths.")
+            print()
+            return
+
+        if len(vector_to_add) == 0 or len(self.__values):
+            print("Vector has no values.")
+            print()
+            return None
+
         i = 0
         while i < len(self.__values):
             self.__values[i] *= vector_to_add[i]
@@ -145,15 +167,6 @@ class VectorRepository(MyVector):
         # initializing the repository
         self.__repository = []
 
-    def get_point_at_index(self, index):
-        # Returns the point found at a given index
-        return self.__repository[index]
-
-    # def get_all_vectors(self):
-        # returns all vectors inside the repository
-        # for item in range(len(self.__repository)):
-        #   self.__repository[item].__str__()
-
     def add_a_vector_to_the_repository(self, new_vector):
         # adds a vector to the repository
         self.__repository = np.append(self.__repository, new_vector)
@@ -163,8 +176,7 @@ class VectorRepository(MyVector):
 
     def get_vector_at_given_index(self, index):
         # get the vector at the given index
-        vector = self.__repository[index]
-        print(vector.__str__())
+        return self.__repository[index]
 
     def get_vectors_having_sum(self, sum):
         # Get the list of vectors having a given sum of elements
@@ -201,13 +213,95 @@ class VectorRepository(MyVector):
 
         self.__repository = result.copy()
 
+    def update_vector_at_given_index(self, index, new_name, new_colour, new_typeInteger, new_values):
+        # update a vector at a given index
+        vector_changed = self.get_vector_at_given_index(index)
+        vector_changed.set_name_id(new_name)
+        vector_changed.set_colour(new_colour)
+        vector_changed.set_type(new_typeInteger)
+        vector_changed.set_values(new_values)
+
+    def update_vector_by_name_id(self, name_id_searched, new_name, new_colour, new_typeInteger, new_values):
+        # updating a vector in the repository by name_id
+        for vector in range(len(self.__repository)):
+            if self.__repository[vector].get_name_id() == name_id_searched:
+                vector_changed = self.get_vector_at_given_index(vector)
+                vector_changed.set_name_id(new_name)
+                vector_changed.set_colour(new_colour)
+                vector_changed.set_type(new_typeInteger)
+                vector_changed.set_values(new_values)
+
+    def delete_vector_at_index(self, index):
+        # deleting a vector in the repository at a given index
+        self.__repository = np.delete(self.__repository, index)
+
+    def delete_vector_by_name(self, name_id_searched):
+        # deleting a vector in the repository by name_id
+        length = len(self.__repository)
+        result = []
+        for vector in range(length):
+            if self.__repository[vector].get_name_id() != name_id_searched:
+                result.append(self.__repository[vector])
+        self.__repository = result.copy()
+
     def add_4_data_example(self):
         # add vectors to the repository
-        self.__repository = np.append(
-            self.__repository, MyVector("v1", "red", 1, [0, 1, 2]))
-        self.__repository = np.append(
-            self.__repository, MyVector("v2", "green", 2, [6, 4, 2]))
-        self.__repository = np.append(
-            self.__repository, MyVector("v3", "pink", 3, [3, 2, 0]))
-        self.__repository = np.append(
-            self.__repository, MyVector("v3", "blue", 4, [3, 7, 8]))
+        length = len(self.__repository)
+        good = 0
+        for vector in range(length):
+            if self.__repository[vector].get_name_id() == "v1" or self.__repository[vector].get_name_id() == "v2" or self.__repository[vector].get_name_id() != "v3" or self.__repository[vector].get_name_id() != "v4":
+                good = 1
+                print(
+                    "One or more of the examples have the name_id already taken, please pick something else instead of: v1, v2 ,v3 or v4!")
+                break
+        if good == 0:
+            self.__repository = np.append(
+                self.__repository, MyVector("ex1", "r", 1, [0, 1, 2]))
+            self.__repository = np.append(
+                self.__repository, MyVector("ex2", "g", 2, [6, 4, 2]))
+            self.__repository = np.append(
+                self.__repository, MyVector("ex3", "y", 3, [3, 2, 0]))
+            self.__repository = np.append(
+                self.__repository, MyVector("ex4", "b", 4, [3, 7, 8]))
+
+    def plot_vectors_in_chart(self):
+        # plot vectors in a chart by types and values and colour
+        for vector in range(len(self.__repository)):
+            point_array = []
+            current_vector = self.__repository[vector]
+            current_type = current_vector.get_type()
+            current_values = current_vector.get_values()
+
+            for point in range(len(current_vector.get_values())):
+                point_array.append(current_values[point])
+
+            points = np.array(point_array)
+            current_type = point_interpretation(current_type)
+            plt.plot(point_array, marker=current_type,
+                     color=current_vector.get_colour())
+
+        plt.show()
+        # Two  lines to make our compiler able to draw:
+        # plt.savefig(sys.stdout.buffer)
+        # sys.stdout.flush()
+
+    def check_id(self, nameId):
+        # check if id already exists in the vector repository
+        length = len(self.__repository)
+        for vector in range(length):
+            if self.__repository[vector].get_name_id() == nameId:
+                return False
+        return True
+
+
+def point_interpretation(current_type):
+    # determine colour based on type and return it
+    if current_type == 1:
+        current_type = "o"
+    elif current_type == 2:
+        current_type = "s"
+    elif current_type == 3:
+        current_type = "v"
+    elif current_type > 3:
+        current_type = "D"
+    return current_type
