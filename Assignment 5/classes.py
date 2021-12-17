6bd70b1425ae1073506ebb5e99de4c6c6535f2d9
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2021-12-17 23:02:36 +0200

    update classes

--- Assignment 5/classes.py
+++ Assignment 5/classes.py
@@ -1,3 +1,6 @@
+from functions import *
+
+
 class Planes:
 
     def __init__(self, id, company, seat_number, destination, passager_list):
@@ -5,7 +8,11 @@ class Planes:
         self.__company = company
         self.__seat_number = seat_number
         self.__destination = destination
-        self.__passager_list = passager_list
+
+        self.__passenger_list_objects = passager_list.copy()
+        self.__passager_list = []
+        for passenger in passager_list:
+            self.__passager_list.append(str(passenger))
 
     def __str__(self):
         # returnig a string containing information about the plane
@@ -42,6 +49,34 @@ class Planes:
         """
         return self.__passager_list
 
+    def get_passager_list_as_object(self):
+        """
+        getter method - get the memory location of the list of passengers
+        """
+        return self.__passenger_list_objects
+
+    def get_passager_list_with_substrings(self, substring):
+        """
+        getter method - get the list of passengers whose first name starts with a given substring
+        """
+        searched_names = []
+        for passenger in self.__passager_list:
+            names = passenger.split()
+            first_name = names[0]
+            if find_substring(first_name, substring):
+                searched_names.append(first_name)
+
+        return searched_names
+
+    def get_string_by_concatenation(self):
+        """
+        getter method - get the string obtained by concatenation of the
+                        number of passengers in the plane and the destination
+        """
+        number_of_passagers = str(len(self.__passager_list))
+        destination = self.__destination
+        return number_of_passagers + destination
+
     # setter methods
     def set_id(self, new_id):
         """
@@ -71,7 +106,24 @@ class Planes:
         """
         setter method - set the list of passengers
         """
-        self.__passager_list = new_passager_list
+        self.__passager_list = new_passager_list.copy()
+
+    def add_passager_to_list(self, passager, index):
+        # add the passager to the list at a given index
+        self.__passager_list[index].append(passager)
+
+    def remove_passager_from_list(self, index):
+        # remove the passager at the given index from the list
+        del self.__passager_list[index]
+
+    # Sorting funtions
+    def sort_passengers_by_last_name(self):
+        # Sort the passengers in a plane by last name
+        passenger_list = self.get_passager_list()
+
+        sortSur(passenger_list)
+
+        self.set_passager_list(passenger_list)
 
 
 class Passenger:
@@ -80,6 +132,10 @@ class Passenger:
         self.__first_name = first_name
         self.__last_name = last_name
         self.__passport_number = passport_number
+
+    def __str__(self):
+        # returnig a string containing information about the passager
+        return f"{self.__first_name} {self.__last_name}"
 
     # getters
     def get_first_name(self):
