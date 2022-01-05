7520cd45de80bae2abe45f4f42fc9907132679e9
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-01-06 00:46:01 +0200

    Update classes.py

--- Assignment 5/classes.py
+++ Assignment 5/classes.py
@@ -1,131 +1,177 @@
-from classes import Planes, Passenger
 from functions import *
 
 
-def commands():
-    print("0  -> quit")
-    print("1  -> Show planes information")
-    print("2  -> Show passagers information for a given plane")
-    print("3  -> Sort the passengers in a plane by last name")
-    print("4  -> Sort planes according to the number of passengers")
-    print("5  -> Sort planes according to the number of passengers with the first name starting with a given substring")
-    print("6  -> Sort planes according to the string obtained by concatenation of the number of passengers in the plane and the destination")
-    print("7  -> Identify planes that have passengers with passport numbers starting with the same 3 letters")
-    print("8  -> Identify passengers from a given plane for which the first name or last name contain a string given as parameter")
-    print("9  -> Identify plane/planes where there is a passenger with given name")
-    print("10 -> Add plane to repository")
+class Planes:
+
+    def __init__(self, id, company, seat_number, destination, passager_list):
+        self.__id = id
+        self.__company = company
+        self.__seat_number = seat_number
+        self.__destination = destination
+
+        self.__passenger_list_objects = passager_list.copy()
+        self.__passager_list = []
+        for passenger in passager_list:
+            self.__passager_list.append(str(passenger))
+
+    def __str__(self):
+        # returnig a string containing information about the plane
+        return f"Plane data: \n Id: {self.__id} \n Company: {self.__company} \n Number of seats: {self.__seat_number} \n Destination: {self.__destination} \n List of passagers: {self.__passager_list}"
+
+    # getter methods
+    def get_id(self):
+        """
+        getter method - get the id of the plane
+        """
+        return self.__id
+
+    def get_company(self):
+        """
+        getter method - get the name of the airline company
+        """
+        return self.__company
+
+    def get_seat_number(self):
+        """
+        getter method - get the number of seats in the plane
+        """
+        return self.__seat_number
+
+    def get_destination(self):
+        """
+        getter method - get the destination 
+        """
+        return self.__destination
+
+    def get_passager_list(self):
+        """
+        getter method - get the list of passengers
+        """
+        return self.__passager_list
+
+    def get_passager_list_as_object(self):
+        """
+        getter method - get the memory location of the list of passengers
+        """
+        return self.__passenger_list_objects
+
+    def get_passager_list_with_substrings(self, substring):
+        """
+        getter method - get the list of passengers whose first name starts with a given substring
+        """
+        searched_names = []
+        for passenger in self.__passager_list:
+            names = passenger.split()
+            first_name = names[0]
+            if find_substring(first_name, substring):
+                searched_names.append(first_name)
+
+        return searched_names
+
+    def get_string_by_concatenation(self):
+        """
+        getter method - get the string obtained by concatenation of the
+                        number of passengers in the plane and the destination
+        """
+        number_of_passagers = str(len(self.__passager_list))
+        destination = self.__destination
+        return number_of_passagers + destination
+
+    # setter methods
+    def set_id(self, new_id):
+        """
+        setter method - set the id of the plane
+        """
+        self.__id = new_id
+
+    def set_company(self, new_company):
+        """
+        setter method - set the name of the airline company
+        """
+        self.__company = new_company
+
+    def set_seat_number(self, new_seat_number):
+        """
+        setter method - set the number of seats in the plane
+        """
+        self.__seat_number = new_seat_number
+
+    def set_destination(self, new_destination):
+        """
+        setter method - set the destination 
+        """
+        self.__destination = new_destination
+
+    def set_passager_list(self, new_passager_list):
+        """
+        setter method - set the list of passengers
+        """
+        self.__passager_list = new_passager_list.copy()
+
+    def add_passager_to_list(self, passager, index):
+        # add the passager to the list at a given index
+        self.__passager_list[index].append(passager)
+
+    def remove_passager_from_list(self, index):
+        # remove the passager at the given index from the list
+        del self.__passager_list[index]
+
+    # Sorting funtions
+    def sort_passengers_by_last_name(self):
+        # Sort the passengers in a plane by last name
+        passenger_list = self.get_passager_list()
+
+        sortSur(passenger_list)
+
+        self.set_passager_list(passenger_list)
 
 
-def main():
+class Passenger:
 
-    passager1 = Passenger("Michael", "Jordan", "j23")
-    passager2 = Passenger("Kobe", "Bryant", "k24")
-    passager3 = Passenger("Rose", "Derrick", "jjj")
-    passager4 = Passenger("Lebron", "James", "jjj")
-    passager5 = Passenger("Kawhi", "Leonard", "kl2")
-    passager6 = Passenger("Jason", "Kidd", "u21")
-    passager7 = Passenger("Tony", "Parker", "t25")
-    passager8 = Passenger("Tim", "Duncan", "tdm")
-    passager9 = Passenger("Larry", "Bird", "lar")
-    passager10 = Passenger("Magic", "Johnson", "mag")
+    def __init__(self, first_name, last_name, passport_number):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__passport_number = passport_number
 
-    planes_repository = [
-        Planes("1", "Tarom", 15, "Cluj-Napoca", [passager1, passager2]),
-        Planes("2", "Tarom", 15, "New York", [passager1]),
-        Planes("3", "Tarom", 30, "London", [passager2, passager3, passager4]),
-        Planes("4", "Tarom", 30, "Paris", [passager2, passager3, passager5]),
-        Planes("5", "Tarom", 25, "Paris", [passager6, passager3, passager5]),
-        Planes("6", "Tarom", 25, "Paris", [passager2, passager6]),
-        Planes("7", "Tarom", 25, "Paris", [passager10]),
-        Planes("8", "Tarom", 25, "Paris", [passager7, passager8, passager10]),
-        Planes("9", "Tarom", 30, "Paris", [passager1, passager9]),
-        Planes("10", "Tarom", 30, "Paris", [passager7, passager9, passager8])
-    ]
+    def __str__(self):
+        # returnig a string containing information about the passager
+        return f"{self.__first_name} {self.__last_name}"
 
-    while True:
-        commands()
+    # getters
+    def get_first_name(self):
+        """
+        getter method - get passager first name
+        """
+        return self.__first_name
 
-        # user picks the command
-        comm = int(input("Your command: "))
+    def get_last_name(self):
+        """
+        getter method - get passager last name
+        """
+        return self.__last_name
 
-        if comm == 0:
-            quit()
+    def get_passport_number(self):
+        """
+        getter method - get passport number
+        """
+        return self.__passport_number
 
-        if comm == 1:
-            # show information about the plane
-            for plane in planes_repository:
-                print(plane.__str__())
+    # setters
+    def set_first_name(self, new_first_name):
+        """
+        setter method - set passager first name
+        """
+        self.__first_name = new_first_name
 
-        if comm == 2:
-            # show information about passengers in a given plane
-            id = int(input("Select plane by id: "))
-            for plane in planes_repository:
-                if plane.get_id() == id:
-                    for passenger in plane.get_passager_list_as_object():
-                        print()
-                        print(f"First name: { passenger.get_first_name()}")
-                        print(f"Last name: { passenger.get_last_name()}")
-                        print(f"Passport: { passenger.get_passport_number()}")
+    def set_last_name(self, new_last_name):
+        """
+        setter method - set passager last name
+        """
+        self.__last_name = new_last_name
 
-        if comm == 3:
-            # Sort the passengers in a plane by last name
-            id = int(input("Select plane by id: "))
-            for plane in planes_repository:
-                if plane.get_id() == id:
-                    plane.sort_passengers_by_last_name()
-
-        if comm == 4:
-            # Sort planes according to the number of passengers
-            bubble_sort(planes_repository)
-
-        if comm == 5:
-            # Sort planes according to the number of passengers with the first name starting with a given substring
-            substring = input("Substring: ")
-            bubble_sort_for_substring_given(planes_repository, substring)
-
-        if comm == 6:
-            # Sort planes according to the string obtained by concatenation
-            # of the number of passengers in the plane and the destination
-            # should be like "3Toronto" n stuff
-            bubble_sort_for_concatenation(planes_repository)
-
-        if comm == 7:
-            # Identify planes that have passengers with
-            # passport numbers starting with the same 3 letters
-            search_planes_by_passport_letters(planes_repository)
-
-        if comm == 8:
-            # Identify passengers from a given plane for which the first name
-            # or last name contain a string given as parameter
-            id = int(input("Select plane by id: "))
-            string_searched = input("String you are looking for: ")
-
-            if len(string_searched) > 0:
-                for plane in planes_repository:
-                    if plane.get_id() == id:
-                        given_plane = plane
-                        break
-                search_passengers_containing_string(
-                    given_plane, string_searched)
-            else:
-                continue
-
-        if comm == 9:
-            # Identify plane/planes where there is a passenger with given name
-            given_name = input("Searched name is: ")
-            search_planes_with_passenger(planes_repository, given_name)
-
-        if comm == 10:
-            # Add plane to repository
-            id = input("Id: ")
-            company = input("Company: ")
-            number_of_seats = int(input("Number of seats: "))
-            destination = input("Destination: ")
-            planes_repository.append(
-                Planes(id, company, number_of_seats, destination, []))
-
-        print()
-
-
-main()
+    def set_passport_number(self, new_passport_number):
+        """
+        setter method - set passport number
+        """
+        self.__passport_number = new_passport_number
 
